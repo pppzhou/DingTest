00304ee643cbb85935c95e2e8d06c26a9098b3a4
zhoupeng <zhoupeng@mininglamp.com>
2020-11-26 15:05:41 +0800

    新增personã.java

---
+++ src/main/java/com/mininglamp/Util/Entrance/Person.java
@@ -0,0 +1,11 @@
+package com.mininglamp.Util.Entrance;
+
+/**
+ * Project: DingTest
+ * Package: com.mininglamp.Util.Entrance
+ * Description
+ * <p>
+ * Created by ZhouPeng on 2020/11/26 14:57
+ **/
+public class Person {
+}
