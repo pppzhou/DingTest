c3bd47718464f242ba572713df97e81cc57ef21a
zhoupeng <zhoupeng@mininglamp.com>
2021-12-17 16:10:27 +0800

    second commit

--- src/test/java/com/mininglamp/gitTest.java
+++ src/test/java/com/mininglamp/gitTest.java
@@ -10,5 +10,6 @@ package com.mininglamp;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git1!");
     }
 }
