ef209f2de2fd093bf04123cef4629f058f7787c1
zhoupeng <zhoupeng@mininglamp.com>
2021-12-17 16:09:11 +0800

    first commit

---
+++ src/test/java/com/mininglamp/gitTest.java
@@ -0,0 +1,14 @@
+package com.mininglamp;
+
+/**
+ * Project: DingTest
+ * Package: com.mininglamp
+ * Description
+ * <p>
+ * Created by ZhouPeng on 2021/12/17 16:08
+ **/
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
