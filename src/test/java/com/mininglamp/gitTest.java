049d59d28452e31a960bd6b8cf990b2c04fbec43
zhoupeng <zhoupeng@mininglamp.com>
2021-12-17 17:23:05 +0800

    push second push test

--- src/test/java/com/mininglamp/gitTest.java
+++ src/test/java/com/mininglamp/gitTest.java
@@ -15,5 +15,6 @@ public class gitTest {
         System.out.println("master test！");
         System.out.println("hot-fix test!");
         System.out.println("push test!");
+        System.out.println("push test1!");
     }
 }
