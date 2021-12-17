222dacc7993dd4ac90d8b683ea4c7584a7d6990b
zhoupeng <zhoupeng@mininglamp.com>
2021-12-17 17:19:28 +0800

    push first commit test

--- src/test/java/com/mininglamp/gitTest.java
+++ src/test/java/com/mininglamp/gitTest.java
@@ -14,5 +14,6 @@ public class gitTest {
         System.out.println("hello git2!");
         System.out.println("master test！");
         System.out.println("hot-fix test!");
+        System.out.println("push test!");
     }
 }
