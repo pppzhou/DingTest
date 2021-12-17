0d16c7c2c1636da9d3fa43a2369039bb3d4ab45a
zhoupeng <zhoupeng@mininglamp.com>
2021-12-17 16:41:27 +0800

    hot-fix commit2

--- src/test/java/com/mininglamp/gitTest.java
+++ src/test/java/com/mininglamp/gitTest.java
@@ -12,5 +12,6 @@ public class gitTest {
         System.out.println("hello git!");
         System.out.println("hello git1!");
         System.out.println("hello git2!");
+        System.out.println("hot-fix test!");
     }
 }
